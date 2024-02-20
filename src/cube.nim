ee7cdd9b7e8b445759038b234910163a2749c8fe
Zaid Fakecrafter <fakecrafter@protonmail.com>
2024-02-20 16:39:44 +0100

    finished cube model

--- src/cube.nim
+++ src/cube.nim
@@ -1,5 +1,4 @@
 type
-  TurnRange = range[0..4]
   Color = enum yellow, white, red, orange, blue, green
   Position = tuple[x: int, y: int, z: int]
   Piece = ref object
@@ -9,9 +8,13 @@ type
     cz: Color
   Cube = array[8, Piece]
 
-const a = @[1,2,3]
+func rotate(p: Position, rotationMatrix: array[3, array[3, int]]): Position =
+  result.x = p.x*rotationMatrix[0][0] + p.y*rotationMatrix[0][1] + p.z*rotationMatrix[0][2]
+  result.y = p.x*rotationMatrix[1][0] + p.y*rotationMatrix[1][1] + p.z*rotationMatrix[1][2]
+  result.z = p.x*rotationMatrix[2][0] + p.y*rotationMatrix[2][1] + p.z*rotationMatrix[2][2]
 
-proc solvedCube(): Cube =
+
+proc newSolvedCube(): Cube =
   let p1: Position = (1,1,1)
   let p2: Position = (1,1,-1)
   let p3: Position = (1,-1,1)
@@ -31,43 +34,51 @@ proc solvedCube(): Cube =
 
 # Y-Rotation
 proc R(c: Cube, rotations: int) =
-  for i in 0 .. rotations mod 4:
+  const rotationY = [[0,0,-1],[0,1,0],[1,0,0]]
+  for i in 0 ..< rotations mod 4:
     for piece in c:
       if piece.pos.y == 1:
-        piece.pos *= rotationY
+        piece.pos = rotate(piece.pos, rotationY)
 
 proc L(c: Cube, rotations: int) =
-  for i in 0 .. rotations mod 4:
+  const rotationY = [[0,0,-1],[0,1,0],[1,0,0]]
+  for i in 0 ..< rotations mod 4:
     for piece in c:
       if piece.pos.y == -1:
-        piece.pos *= rotationY
+        piece.pos = rotate(piece.pos, rotationY)
 
 # Z-Rotation
 proc U(c: Cube, rotations: int) =
-  for i in 0 .. rotations mod 4:
+  const rotationZ = [[0,-1,0],[1,0,0],[0,0,1]]
+  for i in 0 ..< rotations mod 4:
     for piece in c:
       if piece.pos.z == 1:
-        piece.pos *= rotationZ
+        piece.pos = rotate(piece.pos, rotationZ)
 
 proc D(c: Cube, rotations: int) =
-  for i in 0 .. rotations mod 4:
+  const rotationZ = [[0,-1,0],[1,0,0],[0,0,1]]
+  for i in 0 ..< rotations mod 4:
     for piece in c:
       if piece.pos.z == 1:
-        piece.pos *= rotationZ
+        piece.pos = rotate(piece.pos, rotationZ)
 
 # X-Rotation
 proc F(c: Cube, rotations: int) =
-  for i in 0 .. rotations mod 4:
+  const rotationX = [[1,0,0],[0,0,-1],[0,1,0]]
+  for i in 0 ..< rotations mod 4:
     for piece in c:
       if piece.pos.x == 1:
-        piece.pos *= rotationX
+        piece.pos = rotate(piece.pos, rotationX)
 
 proc B(c: Cube, rotations: int) =
-  for i in 0 .. rotations mod 4:
+  const rotationX = [[1,0,0],[0,0,-1],[0,1,0]]
+  for i in 0 ..< rotations mod 4:
     for piece in c:
       if piece.pos.x == 1:
-        piece.pos *= rotationX
-
+        piece.pos = rotate(piece.pos, rotationX)
 
 when isMainModule:
-  echo "Hello World"
+  let a = newSolvedCube()
+  let b = newSolvedCube()
+  if a[0][] == b[0][]:
+    echo "hello"
